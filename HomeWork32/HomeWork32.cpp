3a2810d6ff7f59fb4048c608958f216f22f29cb4
Wait For-Summer <viacheslavterekhov@gmail.com>
2024-10-14 15:59:07 +0300

    Add Discription and new material

--- HomeWork32/HomeWork32.cpp
+++ HomeWork32/HomeWork32.cpp
@@ -1,33 +1,17 @@
-﻿#include <iostream>
+﻿/*
+МГТУ им. Н. Э. Баумана	Домашняя работа номер 2
+Терехов Вячеслав		Группа ИУ8-11
+Вариант 27				Размер обрабатоваемого блока: 4 байта
+Цикл-ий сдвиг вправо	Вел-на сдвига в битах: 5 бит
+*/
+
+#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream> // Для работы с файлами
 #include <typeinfo>
 
 using namespace std;
-
-void Encryption(string file_name) {
-	uint16_t gamma;
-	string input_string;
-	string key;
-
-	cout << "Enter psswd: "; cin >> key;
-	srand(static_cast<unsigned int>(stoi(key))); // Сделали зерно по паролю
-
-	if (!check(key)) {
-		cerr << "Error: Invalid key\n";
-	}
-
-	// cout << stoi(key) << " " << typeid(static_cast<unsigned int>(stoi(key))).name();
-}
-
-void Decryption(string file_name) {
-
-}
-
-void LShift() {}
-
-void RShift() {}
 
 bool check(string key) {
 
@@ -37,7 +21,46 @@ bool check(string key) {
 	if (static_cast<int>(key.size()) > 9) {
 		return false;
 	}
+	return true;
 }
+
+void Encryption(string file_name) {
+	uint16_t gamma = rand();
+
+	string input_string;
+	string key;
+
+	cout << "Enter psswd: "; cin >> key;
+	srand(static_cast<unsigned int>(stoi(key))); // Сделали зерно по паролю
+	if (!check(key)) {
+		cerr << "Error: Invalid key\n";
+	}
+
+	cout << "Enter a message, you would like to encrypt:\n"; cin >> input_string;
+	cout << input_string << "\t" << type(input_string).name();
+
+	vector<char> symbols(input_string.size()); // Создание массива, состоящего из векторов, для создания посимвольной копии строки, поданной на вход программе для щифрования
+	for (int i = 0; i < input_string.size(); i++) {
+		symbols[i] = input_string[i];
+	}
+
+	//if (input_string.size() % 2 == 0) {
+
+	//}
+	//else {
+
+	//}
+
+	cout << gamma << " " << typeid(gamma).name();
+}
+
+void Decryption(string file_name) {
+
+}
+
+void LShift() {}
+
+void RShift() {}
 
 int main(int argc, const char* argv[]) {
 
