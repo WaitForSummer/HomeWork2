3ea316476400bd91f137da878cbe128f59d3956d
Wait For-Summer <viacheslavterekhov@gmail.com>
2024-10-14 13:13:36 +0300

    new structure of project

--- HomeWork32/HomeWork32.cpp
+++ HomeWork32/HomeWork32.cpp
@@ -5,7 +5,26 @@
 
 using namespace std;
 
+void Encryption(string file_name) {
+	uint16_t gamma;
+	string input_string;
+	string key;
+
+	cout << "Enter psswd: "; cin >> key;
+	srand(stoi(key));
+	cout << stoi(key);
+}
+
+void Decryption(string file_name) {
+
+}
+
+void LShift() {}
+
+void RShift() {}
+
 bool check(string key) {
+
 	if (key.empty()) {
 		return false;
 	}
@@ -18,36 +37,23 @@ int main(int argc, const char* argv[]) {
 		cerr << "Program is of the form: " << argv[0] << " <inp1> <inp2>\n";
 		return 5;
 	}
+
 	const string mode(argv[1]); // Режим работы
 	const string file_name(argv[2]); // Имя файла
 
 	//unsigned int key; Ключ-пароль для шифровки или расшифровки
-	string key;
 
 	if (mode != "encryption" && mode != "decryption") { // Проверка на некорректный ввод
-		cout << "Mode name error";
+		cout << "Error: Неправильное название аргумента.";
 		return 1;
 	}
 
 	if (mode == "encryption") { // Шифрование
-		std::ofstream fout(file_name, ios_base::binary); // Создали объект класса ofstream, связали его с file_name и представили его в двоичном виде
-		if (!fout.is_open()) {
-			cerr << "Error: Файл не найден";
-			return 6;
-		}
-		string input_string;
-		cout << "Введите строку, которую хотите зашифровать: "; cin >> input_string;
-		cout << "Введите пароль для шифрования: "; cin >> key;
-		cout << "Введите пароль (целое число от 0 до 10^9 - 1): "; cin >> key;
-		srand(stoi(key)); // Создаем зерно для создания случайных чисел
-		vector<char> symbols(input_string.size()); // Создаем массив длины введенной строки
-		for (int i = 0; i != input_string.size(); i++) {
-			symbols[i] = input_string[i];
-		}
+		cout << "enc";
+		Encryption(file_name);
 	}
 	else if (mode == "decryption") {
 		cout << "dec";
-		return 2;
 	}
 	return 0;
 }
